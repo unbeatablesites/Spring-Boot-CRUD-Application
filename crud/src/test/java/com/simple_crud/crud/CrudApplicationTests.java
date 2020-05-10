ce37bc2a9084e38da2ba881f56e4fb307f64c82c
Frank Uzoka <Frankuzoka@hotmail.com>
2020-05-10 11:49:38 -0400

    Import spring boot

---
+++ crud/src/test/java/com/simple_crud/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple_crud.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
